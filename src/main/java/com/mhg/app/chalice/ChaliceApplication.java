6b2165af3cd4868d00abf9bfb764b255dd6b26c1
ggiande <44848977+ggiande@users.noreply.github.com>
2025-07-22 22:21:15 -0700

    feat: spring boot + redis initializr app

---
+++ src/main/java/com/mhg/app/chalice/ChaliceApplication.java
@@ -0,0 +1,13 @@
+package com.mhg.app.chalice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaliceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaliceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mhg/app/chalice/ChaliceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhg.app.chalice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaliceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
